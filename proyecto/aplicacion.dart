93c799b2afda8c15eb4b43334b4cdb105d0d795d
Jeferson <jefer.hernandez1@gmail.com>
2024-06-20 22:22:46 -0500

    Se expande complejidad, funcionamiento menus, identacion codigo Jeferson & Andres (LiveShare)

--- proyecto/aplicacion.dart
+++ proyecto/aplicacion.dart
@@ -5,12 +5,12 @@ void main() {
   //DECLARACION VARIABLES - LISTAS - MATRICES
   int opcion;      //Variable para preguntar opcion del menu 
   List <String> vectorTemas = [];    //Vector para temas.
-  List <String> vectorComplejidad = ['Facil', 'Medio', 'dificil'];      //Vector para complejidad de los temas 
   String tema;
   int cantTemas;                      //Cantidad de temas que se ingresan
   List <String> vectorEstudiantes = [];  //Vector para temas.
   String estudiantes;
   int cantEstudiantes;
+  List <String> vectorComplejidad = ['Muy facil','Facil', 'Medio', 'dificil'];      //Vector para complejidad de los temas 
 
   //ENTRADA DATOS - PROCESOS FORMULAS - SALIDA DATOS.
   //Punto # 2 Creacion de los temas de exposicion.
@@ -23,35 +23,41 @@ void main() {
     print("Ingrese el tema # ${i+1}:");
     tema = stdin.readLineSync()!;
     vectorTemas.add(tema);
-    print("Ingrese nivel de complejidad para este tema:");//Se tendrán 3 niveles de complejidad (nivel 1-nivel 2-nivel 3)
-     do {
-    print("**********************************************");
-    print("Seleccione el nivel de complejidad:");
-    print("1. Nivel: ${vectorComplejidad[0]}");
-    print("2. Nivel: ${vectorComplejidad[1]}");
-    print("3. Nivel: ${vectorComplejidad[2]}");
-    print("9. Para Salir");
-    print("**********************************************");
-    print("Ingrese la opcion desea:");
-    opcion = int.parse(stdin.readLineSync()!);
+    print("Ingrese nivel de complejidad para este tema # ${i+1} ${vectorTemas[i]}:"); //Se tendrán 3 niveles de complejidad (nivel 1-nivel 2-nivel 3)
+    // do {
+      print("**********************************************");
+      print("Seleccione el nivel de complejidad:");
+      print("1. Nivel: ${vectorComplejidad[0]}");
+      print("2. Nivel: ${vectorComplejidad[1]}");
+      print("3. Nivel: ${vectorComplejidad[2]}");
+      print("4. Nivel: ${vectorComplejidad[3]}");
+      print("9. Para Salir");
+      print("**********************************************");
+      print("Ingrese la opcion deseada:");
+      opcion = int.parse(stdin.readLineSync()!);
 
-    switch (opcion) {
-      case 1:
-        print("Para este tema ${vectorTemas[0]} el nivel de complejidad es ${vectorComplejidad[0]}, se requiere 2 estudiantes");
-      break;
-      case 2: 
-        print("Para este tema ${vectorTemas[1]} el nivel de complejidad es ${vectorComplejidad[1]}, se requiere 3 estudiantes");
-      break;
-      case 3: 
-        print("Para este tema ${vectorTemas[2]} el nivel de complejidad es ${vectorComplejidad[2]}, se requiere 4 estudiantes");
-      break;
-      case 9: 
-        print("Hasta la Proxima");
-      break;
-    }
-  }
-  while ( opcion != 9 );
-  }
+      switch (opcion) {
+        case 1: 
+          print("Para este tema # ${i+1} ${vectorTemas[i]} el nivel de complejidad es ${vectorComplejidad[0]}, se requiere 1 estudiante");
+        break;
+        case 2: 
+          print("Para este tema # ${i+1} ${vectorTemas[i]} el nivel de complejidad es ${vectorComplejidad[1]}, se requiere 2 estudiantes");
+        break;
+        case 3: 
+          print("Para este tema # ${i+1} ${vectorTemas[i]} el nivel de complejidad es ${vectorComplejidad[2]}, se requiere 3 estudiantes");
+        break;
+        case 4:
+          print("Para este tema # ${i+1} ${vectorTemas[i]} el nivel de complejidad es ${vectorComplejidad[3]}, se requiere 4 estudiantes");
+        case 9: 
+          print("Hasta la Proxima");  //Validar si lo quitamos o lo dejamos. 
+        break;
+        default:
+          print("¡Opcion incorrecta!.");
+          print("Ingrese opcion '1,2,3,' o '9 para salir"); 
+      }
+    // } //do
+    // while ( opcion != 9 );   //Verificar DoWhile para opcion 9 salir o no ponerlo el salir. 
+  } //for
   print("Ingrese la cantidad de estudiantes del grupo:");
   cantEstudiantes = int.parse(stdin.readLineSync()!);
   print("Ingrese primero los $cantEstudiantes estudiantes del grupo:"); 
